82fbd1fd6bf653f8d5c39910c9817b53d61d1f3b
YujeongSon <yujeongs24@naver.com>
2022-12-01 21:31:35 +0900

    CreateAccount dto 생성

--- src/main/java/com/example/accountsystem/controller/AccountController.java
+++ src/main/java/com/example/accountsystem/controller/AccountController.java
@@ -1,12 +1,13 @@
 package com.example.accountsystem.controller;
 
 import com.example.accountsystem.domain.Account;
+import com.example.accountsystem.dto.CreateAccount;
 import com.example.accountsystem.service.AccountService;
 import com.example.accountsystem.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,15 +16,15 @@ public class AccountController {
     private final AccountService accountService;
     private final RedisTestService redisTestService;
 
+    @PostMapping("/account")
+    public String createAccount(@RequestBody @Valid CreateAccount.Request request) {
+        accountService.createAccount();
+        return "success";
+    }
+
     @GetMapping("/get-lock")
     public String getLock() {
         return redisTestService.getLock();
-    }
-
-    @GetMapping("/create-account")
-    public String createAccount() {
-        accountService.createAccount();
-        return "success";
     }
 
     @GetMapping("/account/{id}")
