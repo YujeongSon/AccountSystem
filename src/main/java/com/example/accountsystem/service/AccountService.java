2ba0051b0f8771872951b0c52d2d538cc84625c1
YujeongSon <yujeongs24@naver.com>
2022-12-01 21:14:18 +0900

    기본 구조 추가

---
+++ src/main/java/com/example/accountsystem/service/AccountService.java
@@ -0,0 +1,32 @@
+package com.example.accountsystem.service;
+
+import com.example.accountsystem.domain.Account;
+import com.example.accountsystem.domain.AccountStatus;
+import com.example.accountsystem.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public void createAccount() {
+        Account account = Account.builder()
+                .accountNumber("40000")
+                .accountStatus(AccountStatus.IN_USE)
+                .build();
+        accountRepository.save(account);
+    }
+
+    @Transactional
+    public Account getAccount(Long id) {
+        if (id < 0) {
+            throw new RuntimeException("Minus");
+        }
+        return accountRepository.findById(id).get();
+    }
+}
