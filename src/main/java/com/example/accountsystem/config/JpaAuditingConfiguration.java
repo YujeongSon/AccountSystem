21e1aa35c8370a99b24220dd84384375485b3f59
YujeongSon <yujeongs24@naver.com>
2022-12-01 22:18:03 +0900

    JpaAuditingConfiguration 추가

---
+++ src/main/java/com/example/accountsystem/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.accountsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
