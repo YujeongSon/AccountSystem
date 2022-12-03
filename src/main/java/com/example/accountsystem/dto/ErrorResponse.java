ed3db0faa3829ab702ea6ad5584d99cf4e3d37bd
YujeongSon <yujeongs24@naver.com>
2022-12-03 10:00:51 +0900

    예외 처리

---
+++ src/main/java/com/example/accountsystem/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.accountsystem.dto;
+
+import com.example.accountsystem.type.ErrorCode;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String errorMessage;
+}
